7c1e728f832b32cdb9c389cca477bf7d56833654
josemazhuvanchery1 <jose1998mathew@gmail.com>
2023-02-08 16:42:56 -0500

    landing page styling updated

--- home-ease/src/components/Searchbox.tsx
+++ home-ease/src/components/Searchbox.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Search } from 'grommet-icons';
-import { Grommet, Box, TextInput } from 'grommet';
+import React, { useState } from 'react'
+import { Search, Location} from 'grommet-icons';
+import { Grommet, Box, TextInput, Select, Button  } from 'grommet';
+import '../searchBox.css'
 const Searchbox = () => {
     const theme = {
         global: {
@@ -11,13 +12,76 @@ const Searchbox = () => {
             },
         },
     };
+   
+
+    const Options: string[] = [];
+    for (let i = 1; i <= 5; i += 1) {
+      Options.push(`option ${i}`);
+    }
+    
+    const maxPrices: string[] = [];
+    for(let i = 6; i<=10; i++){
+        maxPrices.push(`option ${i}`)
+    }
+    
+    const [minOptions, minSetOptions] = useState(Options);
+  const [value, minSetValue] = useState('');
+ 
+  const [maxOptions, maxSetOptions] = useState(maxPrices)
+  const [maxValue, maxSetValue] = useState('')
+  
+  console.log(minOptions)
+
     return (
         <Grommet theme={theme}>
-            <Box fill align="center" justify="start" pad="large">
-                <Box width="medium" gap="medium">
-                    <TextInput icon={<Search />} reverse placeholder="search ..." />
-                </Box>
-            </Box>
+            <div className='searchBox'>
+                <div className='location'>
+                    <div>Location</div>
+                    <Box fill align="center" justify="start" >
+                        <Box width="medium" gap="medium">
+                            <TextInput icon={<Location />} placeholder="search for City, Neighborhood or Zip" />
+                        </Box>
+                    </Box>
+                </div>
+                <input name="area[]" type="hidden" value="401"></input>
+                <div className='dropDowns'>
+                    <div className='minPrice'>
+                        <label id="minPriceLabel" className='min_price'>Minimum Price</label>
+                        <Box fill align="center" justify="start" >
+                            <Select
+                                size="small"
+                                placeholder="Select"
+                                value={value}
+                                options={minOptions}
+                                onChange={({ option }) => {
+                                    
+                                        minSetValue(option);
+                                    
+                                }}
+                                onClose={() => minSetOptions(minOptions)}
+                            />
+                        </Box>
+                    </div>
+                    <div className='maxPrice'>
+                        <label id="maxPriceLabel" className='max_price'>Maxmimum Price</label>
+                        <Box fill align="center" justify="start" >
+                            <Select
+                                size="small"
+                                placeholder="Select"
+                                value={maxValue}
+                                options={maxOptions}
+                                onChange={({ option }) => {
+                                        maxSetValue(option);
+                                    
+                                }}
+                                onClose={() => maxSetOptions(maxOptions)}
+                            />
+                        </Box>
+                    </div>
+                </div>
+                {/* <Button icon={<Search />}  onClick={() => {}} /> */}
+                <Button size="large" icon={<Search />} primary />
+            </div>
         </Grommet>
     )
 }
